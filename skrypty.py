aafc707b2a5d9828e0a15381375e900bfe521ce4
Michalisnot <01169812@pw.edu.pl>
2024-05-14 07:35:44 +0200

    Signed-off-by: Michalisnot <01169812@pw.edu.pl>

--- skrypty.py
+++ skrypty.py
@@ -1,10 +1,10 @@
-from math import sin, cos, sqrt, atan, atan2, degrees, radians
+from math import sin, cos, sqrt, atan, atan2, degrees, radians, log,tan,pi
 from pprint import pprint
 import numpy as np
 o = object()
 import sys
 class Transformacje:
-    def __init__(self, model: str = "wgs84"):
+    def __init__(self, model: str = "grs80"):
         """
         Parametry elipsoid:
             a - duża półoś elipsoidy - promień równikowy
@@ -31,7 +31,9 @@ class Transformacje:
         self.ecc2 = (2 * self.flat - self.flat ** 2) # eccentricity**2
 
 
-    
+    def r_curv(self,f):
+        N = self.a / np.sqrt(1 - self.ecc2 * np.sin(f)**2)
+        return (N)
     def xyz2plh(self, X, Y, Z, output = 'dec_degree'):
         """
         Algorytm Hirvonena - algorytm transformacji współrzędnych ortokartezjańskich (x, y, z)
@@ -54,17 +56,19 @@ class Transformacje:
             dec_degree - decimal degree
             dms - degree, minutes, sec
         """
-        r   = sqrt(X**2 + Y**2)           # promień
-        lat_prev = atan(Z / (r * (1 - self.ecc2)))    # pierwsze przybliilizenie
-        lat = 0
-        while abs(lat_prev - lat) > 0.000001/206265:    
-            lat_prev = lat
-            N = self.a / sqrt(1 - self.ecc2 * sin(lat_prev)**2)
-            h = r / cos(lat_prev) - N
-            lat = atan((Z/r) * (((1 - self.ecc2 * N/(N + h))**(-1))))
-        lon = atan(Y/X)
-        N = self.a / sqrt(1 - self.ecc2 * (sin(lat))**2);
-        h = r / cos(lat) - N       
+        l = atan2(Y,X)
+        p = np.sqrt(X**2 + Y**2)
+        f = atan(Z / (p * (1 - self.ecc2)))
+        while True:
+            N = self.a / np.sqrt(1 - self.ecc2 * np.sin(f)**2)
+            h = (p / np.cos(f))- N
+            fs = f
+            f = np.arctan(Z / (p * (1 - (self.ecc2 * (N / ( N + h))))))
+            if np.abs(fs-f) < (0.000001/206265):
+                break
+        l = np.arctan2(Y,X)
+        lat = f
+        lon = l
         if output == "dec_degree":
             return degrees(lat), degrees(lon), h 
         elif output == "dms":
@@ -73,19 +77,18 @@ class Transformacje:
             return f"{lat[0]:02d}:{lat[1]:02d}:{lat[2]:.2f}", f"{lon[0]:02d}:{lon[1]:02d}:{lon[2]:.2f}", f"{h:.3f}"
         else:
             raise NotImplementedError(f"{output} - output format not defined")
-            
     def plh2xyz(self, phi, lam, h):
-        
+       # r = a * sqrt((1 - e2) / 1 - e2 * (np.cos(f) ** 2))
         phi = radians(phi)
         lam = radians(lam)
         
-        Rn = self.a/sqrt(1-self.ecc2*sin(phi)**2)
+        N = self.a / np.sqrt(1 - self.ecc2 * np.sin(phi)**2)
         q = Rn*self.ecc2*sin(phi)
-        
-        x = (Rn + h)*cos(phi)*cos(lam)
-        y = (Rn + h)*cos(phi)*sin(lam)
-        z = (Rn + h)*sin(phi) - q
-        return x,y,z
+        x = (N + h) * np.cos(phi) * np.cos(lam)
+        y = (N + h) * np.cos(phi) * np.sin(lam)
+        z = (N * (1-self.ecc2) + h) * np.sin(phi)       
+        return (x,y,z)        
+
     #zajęcia 6.05
 
     def xyz2neu(self,x,y,z,x_0,y_0,z_0):
@@ -104,11 +107,89 @@ class Transformacje:
         
         return [float(enu[1]),float(enu[0]),float(enu[2])]
         
+    
+    # def bdl2pdl(self,b,l):
+    #     U = 1 + self.ecc2*sin(b)
+    #     V = 1 - self.ecc2*sin(b)
+    #     K = (U/V)**(self.ecc/2)
+    #     C = K * tan(b/2 +pi/4)
+    #     phi = 2*atan2(C,1)-pi/2
+    #     dl = l
+    #     return phi, dl
+    # def pdl2xymerc(self, p, dl):
+    #     p = sin(p)
+    #     q = cos(p)*cos(dl)
+    #     r = 1 + cos(p)*sin(dl)
+    #     s = 1 - cos(p)*sin(dl)
+    #     R0=6367449.14577
+    #     x_merc=R0 *atan2(p,q)
+    #     y_merc=0.5*R0*log(r/s)
+    #     return x_merc, y_merc
+    # def xymerc2xygk(self,x_merc,y_merc):
+    #     s = 2.0e-6
+    #     x_0 = 5760000.000
+    #     zz = [(x_merc-x_0)*s,y_merc*s]
+    #     a_0 = 5765181.11148097
+    #     a_1 = 499800.817138
+    #     a_2 = -63.81145283
+    #     a_3 = 0.83547915
+    #     a_4 = 0.13046891
+    #     a_5 = -0.00111138
+    #     a_6 = -0.00010504
+        
+    #     z_gk = [a_0+z*(a_1+z*(a_2+z*(a_3+z*(a_4+z*(a_5+z*(a_6+z)))))) for z in zz]
+        
+        
+        
+        
+    #     return(z_gk)
+    
+    # def xygk2xy1992(self,x_gk,y_gk):
+    #     m_0 = 0.9993
+    #     # x_0 = -5300000
+    #     y_0 = 500000
+    #     x_1992 = m_0*x_gk+x_0
+    #     y_1992 = m_0*y_gk+y_0
+    #     return(x_1992,y_1992)
+    # def bl2xy1992(self,b,l):
+    #     l_0 = radians(19)
+    #     dl = l-l_0
+    #     p,dl  =  self.bdl2pdl(b, dl)
+    #     x_merc, y_merc = self.pdl2xymerc(p, dl)
+    #     x_gk, y_gk=  self.xymerc2xygk(x_merc, y_merc)
+    #     x_1992,y_1992 =  self.xygk2xy1992(x_gk, y_gk)
+    #     return(x_1992,y_1992)
+    def sigma(self,f):
+        a = self.a
+        e2  = self.ecc2
+        A0 = 1 - e2/4 - 3 * e2**2/64 - 5 * e2**3 / 256
+        A2 = (3/8) * (e2 + e2**2 / 4 + 15 * e2**3 / 128)
+        A4 = (15/256) * (e2**2 + 3 * e2**3 / 4)
+        A6 = 35 * e2**3 / 3072
+        sig = a * (A0 * f - A2*np.sin(2*f) + A4*np.sin(4*f) - A6*np.sin(6*f))
+        return(sig)
+    
+    def GK(self,f,l,l0):
+        a = self.a
+        e2  = self.ecc2
+        b = np.sqrt(a**2 * (1-e2))
+        ep2 = (a**2 - b**2)/b**2
+        
+        dl = l - l0
+        t = np.tan(f)
+        eta2 = ep2 * np.cos(f)**2
+        N = self.r_curv(f)
+        
+        sig = self.sigma(f)
+        
+        x_gk = sig + dl**2 / 2 * N * np.sin(f) * np.cos(f) * (1 + dl**2 / 12 * np.cos(f)**2 * (5 - t**2 + 9*eta2 + 4*eta2**2) + dl**4 / 360 * np.cos(f)**4 *(61 - 58*t**2 + t**4 + 270*eta2 - 330*eta2*t**2))
+        y_gk = dl*N*np.cos(f)*(1 + dl**2/6*np.cos(f)**2*(1 - t**2 + eta2) + dl**4/120*np.cos(f)**4*(5 - 18*t**2 + t**4 + 14*eta2 - 58*eta2*t**2))
+        return(x_gk, y_gk)  
         
 
 if __name__ == "__main__":
     # utworzenie obiektu
-    geo = Transformacje(model = "wgs84")
+    geo = Transformacje(model = "grs80")
     
     print(sys.argv)
     
@@ -176,54 +257,58 @@ if __name__ == "__main__":
          
     # dane XYZ geocentryczne
     X = 3664940.500; Y = 1409153.590; Z = 5009571.170
+    x0 = 10000; y0 = 5000; z0 = 1000
+    
     phi, lam, h = geo.xyz2plh(X, Y, Z)
-    print(phi, lam, h)
-    # phi, lam, h = geo.xyz2plh2(X, Y, Z)
     # print(phi, lam, h)
+    n,e,u = geo.xyz2neu(X, Y, Z, x0, y0, z0)
+    print(n,e,u)
+
+
     coords_plh=[]
-with open('wsp_inp.txt') as f:
-    lines = f.readlines()
-    lines = lines[4:]
-    for line in lines:
-        line = line.strip('\n')
-        x_str,y_str,z_str= line.split(',')
-        x,y,z = (float(x_str),float(y_str),float(z_str))
-        p,l,h=geo.xyz2plh(x,y,z)
-        coords_plh.append([p,l,h])
-print(coords_plh)
-with open('Result_xyz2plh.txt', 'w') as f:
+# with open('wsp_inp.txt') as f:
+#     lines = f.readlines()
+#     lines = lines[4:]
+#     for line in lines:
+#         line = line.strip('\n')
+#         x_str,y_str,z_str= line.split(',')
+#         x,y,z = (float(x_str),float(y_str),float(z_str))
+#         p,l,h=geo.xyz2plh(x,y,z)
+#         coords_plh.append([p,l,h])
+# print(coords_plh)
+# with open('Result_xyz2plh.txt', 'w') as f:
     
-    f.write('phi[deg],        lambda[deg],          H[m]\n')
-    
-    
-    # coords_plh_line = ','.join([str(coord)for coord in coords])
-    
-    coords_plh_line= '\n'.join([str(coords)for coords in coords_plh])
-    
-    f.writelines(coords_plh_line)
+#     f.write('phi[deg],        lambda[deg],          H[m]\n')
     
     
-coords_xyz=[]
-with open('Result_xyz2plh.txt') as f:
-        lines = f.readlines()
-        lines = lines[1:]
-        for line in lines:
-            line = line.strip('\n')
-            phi_str,lam_str,h_str = line.split(',')
-            phi,lam,h = (float(x_str),float(y_str),float(z_str))
-            x,y,z = geo.plh2xyz(phi,lam,h)
-            coords_xyz.append([x,y,z])
-    # print(coords_plh)
-with open('Result_plh2xyz.txt', 'w') as f:
+#     # coords_plh_line = ','.join([str(coord)for coord in coords])
     
-    f.write('x[m],        y[m],          z[m]\n')
+#     coords_plh_line= '\n'.join([str(coords)for coords in coords_plh])
+    
+#     f.writelines(coords_plh_line)
     
     
-    # coords_plh_line = ','.join([str(coord)for coord in coords])
+# coords_xyz=[]
+# with open('Result_xyz2plh.txt') as f:
+#         lines = f.readlines()
+#         lines = lines[1:]
+#         for line in lines:
+#             line = line.strip('\n')
+#             phi_str,lam_str,h_str = line.split(',')
+#             phi,lam,h = (float(x_str),float(y_str),float(z_str))
+#             x,y,z = geo.plh2xyz(phi,lam,h)
+#             coords_xyz.append([x,y,z])
+#     # print(coords_plh)
+# with open('Result_plh2xyz.txt', 'w') as f:
     
-    coords_xyz_line2= '\n'.join([str(coords)for coords in coords_xyz])
+#     f.write('x[m],        y[m],          z[m]\n')
     
-    f.writelines(coords_xyz_line2)
+    
+#     # coords_plh_line = ','.join([str(coord)for coord in coords])
+    
+#     coords_xyz_line2= '\n'.join([str(coords)for coords in coords_xyz])
+    
+#     f.writelines(coords_xyz_line2)
     
     
     
@@ -237,9 +322,9 @@ x,y,z = 1,1,1
 x_0,y_0,z_0 = 1,1,3
 XD = geo.xyz2neu(x,y,z,x_0,y_0,z_0)    
     
-    
 
-    
+G = geo.bl2xy1992(0.9092689315350225, 0.3670695034002574)
+
     
     
     
